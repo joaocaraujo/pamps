63ac9e91af650c241f2b2e38c8a832b85726097c
Joao Araujo <araujo_joaol@yahoo.com.br>
2025-12-07 12:45:41 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen bg-gray-50">
+    <div class="container mx-auto px-4 py-16">
+      <h1 class="text-4xl font-bold text-center text-gray-900 mb-8">
+        INITIAL Projeto Pamps
+      </h1>
+    </div>
+  </div>
+</template>
+
